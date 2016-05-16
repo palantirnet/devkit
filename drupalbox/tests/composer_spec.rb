321edc9078b643652c72eb8adb81f3ddd040f12c
Mathew Peterson <peterson@palantir.net>
2016-05-16 11:50:14 -0500

    Adds test for adding composer to PATH

--- drupalbox/tests/composer_spec.rb
+++ drupalbox/tests/composer_spec.rb
@@ -4,3 +4,7 @@ describe file('/usr/local/bin/composer') do
     it { should exist }
     it { should be_executable }
 end
+
+describe file('/home/vagrant/.bashrc') do
+    it { should contain 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' }
+end
