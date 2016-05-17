c144c51290569fc4eb88a93d0092f97502529248
Mathew Peterson <peterson@palantir.net>
2016-05-17 11:06:18 -0500

    Fixes spec_helper for drupalbox to allow test from packer build

--- drupalbox/tests/spec_helper.rb
+++ drupalbox/tests/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'serverspec'
 require 'net/ssh'
 
-set :backend, :ssh
+set :backend, :exec
 
 # Set PATH (OEL 5 does not include /sbin by default)
 set :path, '/usr/local/sbin:/usr/sbin:/sbin:$PATH'
