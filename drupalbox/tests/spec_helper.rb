084c088ac8cd446ec2943bae0b089d35e063a012
Mathew Peterson <peterson@palantir.net>
2016-05-20 11:26:48 -0500

    Fixes Vagrantfile to comment out test provisioner

--- drupalbox/tests/spec_helper.rb
+++ drupalbox/tests/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'serverspec'
 require 'net/ssh'
 
+# Use the ssh backend when testing via Vagrant
+# set :backend, :ssh
+
+# Use the exec backend when testing via Packer
 set :backend, :exec
 
 # Set PATH (OEL 5 does not include /sbin by default)
