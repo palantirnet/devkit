a05cd06c0745bc055a08de8b5fb9d46acf25aef2
Mathew Peterson <peterson@palantir.net>
2016-06-09 09:52:17 -0500

    Adds build deps for virtualbox provider

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -4,9 +4,8 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
-    # Assuming the following packages are installed
-    # apt-get install -y linux-headers-$(uname -r) build-essential perl
-    # apt-get install -y dkms
+    apt-get install -y linux-headers-$(uname -r) build-essential perl
+    apt-get install -y dkms
 
     VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
     mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
