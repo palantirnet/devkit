366a616c0fe89c16b3b0630e2402318207492534
Bec White <white@palantir.net>
2017-10-10 14:26:18 -0500

    Only build for one Ubuntu image; add missing extensions for PHP 7.

--- drupalbox/tests/php_spec.rb
+++ drupalbox/tests/php_spec.rb
@@ -1,15 +1,11 @@
 require_relative 'spec_helper'
 
 # Different PHP cli packages should be installed depending on the Ubuntu version.
-describe package('php5.6-cli'), :if => os[:release] == '14.04' do
+describe package("php7.0-cli") do
   it { should be_installed }
 end
 
-describe package("php7.0-cli"), :if => os[:release] == '16.04' do
-  it { should be_installed }
-end
-
-# These PHP extensions should be installed and enabled for either PHP 5 or PHP 7.
+# These PHP extensions should be installed and enabled
 %w{
   curl
   gd
@@ -19,13 +15,11 @@ end
   mcrypt
   sqlite3
   xml
+  memcached
+  xdebug
 }.each do |pkg|
   # The PHP extension's package should be installed from apt.
-  describe package("php5.6-#{pkg}"), :if => os[:release] == '14.04' do
-    it { should be_installed }
-  end
-
-  describe package("php7.0-#{pkg}"), :if => os[:release] == '16.04' do
+  describe package("php7.0-#{pkg}") do
     it { should be_installed }
   end
 
@@ -35,59 +29,27 @@ end
   end
 end
 
-if os[:release] == '14.04'
-  # These packages are installed from apt, but have a different naming pattern
-  # than the other PHP 5.6 extensions above.
-  %w{
-    memcached
-    xdebug
-  }.each do |pkg|
-    # The PHP extension's package should be installed from apt.
-    describe package("php-#{pkg}") do
-      it { should be_installed }
-    end
-  end
-
-  # xdebug is enabled for apache2
-  describe file('/etc/php/5.6/apache2/conf.d/20-xdebug.ini') do
-      it { should be_symlink }
-  end
-
-  # xdebug is disabled for cli
-  describe file('/etc/php/5.6/cli/conf.d/20-xdebug.ini') do
-      it { should_not exist }
-  end
-
-  # The memcached PHP extension is loaded.
-  context php_extension('memcached') do
-    it { should be_loaded }
-  end
-
-  # The yaml extension is installed from pecl, not apt, so here we just check
-  # that it's enabled.
-  context php_extension('yaml') do
-    it { should be_loaded }
-  end
-
-  # The MySQL extension uses different naming patterns between PHP 5 and 7.
-  describe package("php5.6-mysql") do
-    it { should be_installed }
-  end
-
-  context php_extension("mysql") do
-    it { should be_loaded }
-  end
+# The MySQL extension uses different naming patterns between PHP 5 and 7.
+describe package("php7.0-mysql") do
+  it { should be_installed }
 end
 
-if os[:release] == '16.04'
-  # The MySQL extension uses different naming patterns between PHP 5 and 7.
-  describe package("php7.0-mysql") do
-    it { should be_installed }
-  end
+context php_extension("mysqli") do
+  it { should be_loaded }
+end
 
-  context php_extension("mysqli") do
-    it { should be_loaded }
-  end
+# XDebug should be enabled for Apache, but disabled for the CLI
+describe file('/etc/php/7.0/apache2/conf.d/20-xdebug.ini') do
+    it { should be_symlink }
+end
+describe file('/etc/php/7.0/cli/conf.d/20-xdebug.ini') do
+    it { should_not exist }
+end
+
+# The yaml extension is installed from pecl, not apt, so here we just check
+# that it's enabled.
+context php_extension('yaml') do
+  it { should be_loaded }
 end
 
 describe 'PHP config parameters' do
