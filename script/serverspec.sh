332fdc5f1a21392a025d7a55cc29f3b92adb3889
Bec White <white@palantir.net>
2017-10-24 12:05:10 -0500

    rvm is installed under the vagrant user, so hackishly add the rvm install path binaries for the serverspec tests, which are run as root.

--- script/serverspec.sh
+++ script/serverspec.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+export PATH="$PATH:/home/vagrant/.rvm/rubies/ruby-2.3.1/bin"
 gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install --path=vendor
-sudo bundle exec rake spec
+bundle exec rake spec
