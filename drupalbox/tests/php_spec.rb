ec825227d6f07d65077fd00cf2ace86e76c8838b
Bec White <white@palantir.net>
2018-07-25 11:40:37 -0500

    Update serverspec test for PHP 7.2

--- drupalbox/tests/php_spec.rb
+++ drupalbox/tests/php_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 # Different PHP cli packages should be installed depending on the Ubuntu version.
-describe package("php7.1-cli") do
+describe package("php7.2-cli") do
   it { should be_installed }
 end
 
@@ -16,7 +16,7 @@ end
   xml
 }.each do |pkg|
   # The PHP extension's package should be installed from apt.
-  describe package("php7.1-#{pkg}") do
+  describe package("php7.2-#{pkg}") do
     it { should be_installed }
   end
 
@@ -34,14 +34,14 @@ context php_extension("memcached") do
   it { should be_loaded }
 end
 
-describe package("php7.1-mcrypt") do
+describe package("php7.2-mcrypt") do
   it { should be_installed }
 end
 context php_extension("mcrypt") do
   it { should be_loaded }
 end
 
-describe package("php7.1-mysql") do
+describe package("php7.2-mysql") do
   it { should be_installed }
 end
 context php_extension("mysqli") do
@@ -52,10 +52,10 @@ end
 describe package("php-xdebug") do
   it { should be_installed }
 end
-describe file('/etc/php/7.1/fpm/conf.d/20-xdebug.ini') do
+describe file('/etc/php/7.2/fpm/conf.d/20-xdebug.ini') do
     it { should be_symlink }
 end
-describe file('/etc/php/7.1/cli/conf.d/20-xdebug.ini') do
+describe file('/etc/php/7.2/cli/conf.d/20-xdebug.ini') do
     it { should_not exist }
 end
 
