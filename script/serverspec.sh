081bc78a6e1503c0cd63c0c3bd4f8ce9fc21d6ad
Bec White <white@palantir.net>
2020-02-04 17:03:45 -0600

    Update ruby version used by serverspec.

--- script/serverspec.sh
+++ script/serverspec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH="$PATH:/home/vagrant/.rvm/rubies/ruby-2.3.1/bin"
+export PATH="$PATH:/home/vagrant/.rvm/rubies/ruby-2.5.2/bin"
 gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install --path=vendor
