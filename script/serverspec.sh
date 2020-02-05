abd6b3a789dc3c32ee1fda4f3f435136a1303e6c
Bec White <white@palantir.net>
2020-02-04 18:05:19 -0600

    Don't install bundler (it's already provided by the rvm-installed ruby)

--- script/serverspec.sh
+++ script/serverspec.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 export PATH="$PATH:/home/vagrant/.rvm/rubies/ruby-2.5.2/bin"
-gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install --path=vendor
 bundle exec rake spec
