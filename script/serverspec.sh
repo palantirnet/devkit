ca83df1d1c7bdf53353ed72198e842b2caefbdd1
Bec White <white@palantir.net>
2020-02-07 19:31:17 -0600

    Update how rvm is used by the serverspec.sh script.

--- script/serverspec.sh
+++ script/serverspec.sh
@@ -1,5 +1,14 @@
-#!/bin/bash -x
-export PATH="$PATH:/home/vagrant/.rvm/rubies/ruby-2.5.2/bin"
-cd /tmp/tests
+#!/bin/bash
+# This script is run during the Packer build process.
+
+# Wherever the tests are expected to live.
+tests_path="/tmp/tests"
+specs_path="/tmp/tests/specs/*_spec.rb"
+
+# Load rvm into this session
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# Run serverspec
+cd "$tests_path"
 bundle install --path=vendor
-bundle exec rake spec
+bundle exec rake spec["$specs_path"]
